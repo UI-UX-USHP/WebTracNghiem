12569673a41715378a4fe86a23166b6550a239b8
UI-UX-USHP <tranthienphub8d5555attt@gmai.com>
2024-11-26 09:28:29 +0700

    Cập nhật Home và lây out

--- TracNghiem/wwwroot/js/site.js
+++ TracNghiem/wwwroot/js/site.js
@@ -1,4 +1,7 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+function playVideo() {
+    alert("This is a demo video play functionality!");
+}
 
 // Write your JavaScript code.
